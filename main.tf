43e805f4d6b0aad73bce70607dee34a03d65616a
Srishti Oberoi <srishtioberoi01@gmail.com>
2025-08-08 17:43:07 +0530

    adding variable.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 variable "RG1" {
   default = {
-  "srishti" = "Eastus"
-  "gaurav"  = "Westeurope"
-  "hitesh"  = "Westus"
-  "oberoi"  = "Eastus"
+  "RG-dev-01" = "Eastus"
+  "RG-dev-02"  = "Westeurope"
+  "RG-dev-03"  = "Westus"
+  "RG-dev-04"  = "Eastus"
 
   }
 }
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "RG1" {
+  default = {
+  "RG-dev-01" = "Eastus"
+  "RG-dev-02"  = "Westeurope"
+  "RG-dev-03"  = "Westus"
+  "RG-dev-04"  = "Eastus"
+
+  }
+}
